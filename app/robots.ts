0069b47f9a0782e8ea7ec0265ae29a900b0c7186
Yusuf <yusufahmad0350@gmail.com>
2026-02-13 18:38:56 +0530

    delete

--- app/robots.ts
+++
@@ -1,8 +0,0 @@
-import { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: [{ userAgent: "*", allow: "/" }],
-    sitemap: "https://your-domain.com/sitemap.xml",
-  };
-}
